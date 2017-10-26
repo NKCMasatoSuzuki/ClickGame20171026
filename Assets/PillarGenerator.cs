55d4fdc64be3fa4f21355e1775cc217b729ef480
Masato Suzuki <nkc.Masato.Suzuki@gmail.com>
2017-10-26 11:09:05 +0900

    柱削除

---
+++ Assets/PillarGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < 20; i++) {
+            for(int j = 0; j < 20; j++) {
+                GameObject Pillar = Instantiate(PillarPrefab) as GameObject;
+                Pillar.transform.position = new Vector3(i, 0, j);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
